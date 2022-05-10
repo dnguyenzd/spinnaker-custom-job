975e787eedac73aa47641ffb7e3ad49f242656b4
David Nguyen <david.nguyen@zendesk.com>
2022-05-10 22:32:35 +1000

    Output artifact correctly

--- generate.sh
+++ generate.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 BASE64_MANIFEST=$(jsonnet -S ${SCRIPT_DIR}/deployment.jsonnet | base64 | tr -d '\n')
-echo 'SPINNAKER_CONFIG_JSON={"type": "embedded/base64", "reference": "'${BASE64_MANIFEST}'", "name": "deployment.yml"}'
+echo 'SPINNAKER_CONFIG_JSON={"artifacts":[{"type": "embedded/base64", "reference": "'${BASE64_MANIFEST}'", "name": "deployment.yml"}]}'
