3624a5a0404920aec9c1cc752f77ddd35cf36423
David Nguyen <david.nguyen@zendesk.com>
2022-05-10 21:49:42 +1000

    Fix wrong tr command

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-BASE64_MANIFEST=$(jsonnet -S ${SCRIPT_DIR}/deployment.jsonnet | base64 | tr -d '\b')
+BASE64_MANIFEST=$(jsonnet -S ${SCRIPT_DIR}/deployment.jsonnet | base64 | tr -d '\n')
 echo 'SPINNAKER_CONFIG_JSON={"type": "embedded/base64", "reference": "'${BASE64_MANIFEST}'", "name": "deployment.yml"}'
