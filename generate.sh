f66a5b2f9ba9d5040997bd504a69249c7e7a3faf
David Nguyen <david.nguyen@zendesk.com>
2022-05-10 21:38:36 +1000

    Find jsonnet file from script location

--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-BASE64_MANIFEST=$(jsonnet -S deployment.jsonnet | base64 | tr -d '\b')
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+BASE64_MANIFEST=$(jsonnet -S ${SCRIPT_DIR}/deployment.jsonnet | base64 | tr -d '\b')
 echo 'SPINNAKER_CONFIG_JSON={"type": "embedded/base64", "reference": "'${BASE64_MANIFEST}'", "name": "deployment.yml"}'
